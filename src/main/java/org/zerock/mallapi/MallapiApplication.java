d530352d9cb5e491d8d5fdee0159c149ffca0eda
pull-sh <mjoy123@naver.com>
2025-12-02 14:57:39 +0900

    Update MallapiApplication.java

--- src/main/java/org/zerock/mallapi/MallapiApplication.java
+++ src/main/java/org/zerock/mallapi/MallapiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MallapiApplication {
  
 	public static void main(String[] args) {
-		//test comment
 		SpringApplication.run(MallapiApplication.class, args);
 	}
 
